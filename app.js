112fadd3058d35aa2f9e186d60a493c907f9f261
Steavn Chen <tom9876555@yahoo.com.tw>
2022-10-15 00:43:04 +0800

    Project reorganization

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
   res.locals.user = getUser(req)
+  res.locals.current_user = getUser(req)
   next()
 })
 
